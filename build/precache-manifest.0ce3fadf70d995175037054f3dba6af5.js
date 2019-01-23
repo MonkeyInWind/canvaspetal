413802876659ff9c3a9f1ec0c39d9cd613015c4e
MonkeyInWind <xlyytcy_pyc@outlook.com>
2019-01-23 09:27:51 +0800

    build

---
+++ build/precache-manifest.0ce3fadf70d995175037054f3dba6af5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8faea81946f3cff8915b",
+    "url": "/canvaspetal/static/js/runtime~main.8faea819.js"
+  },
+  {
+    "revision": "3e2de6c1f0a63ba08a2f",
+    "url": "/canvaspetal/static/js/main.3e2de6c1.chunk.js"
+  },
+  {
+    "revision": "4e5ec32ebd5d66e98e5a",
+    "url": "/canvaspetal/static/js/1.4e5ec32e.chunk.js"
+  },
+  {
+    "revision": "3e2de6c1f0a63ba08a2f",
+    "url": "/canvaspetal/static/css/main.e1d09555.chunk.css"
+  },
+  {
+    "revision": "e320b626977c19d38b43a2eeeef55a19",
+    "url": "/canvaspetal/index.html"
+  }
+];
